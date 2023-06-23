c7fd5a8fa4d340ba3eddb0768131208f33407295
Svetoslav Georgiev <s.georgiev@kodar.net>
2023-06-23 22:50:37 +0300

    update unit tests

--- src/app/pages/details/details.component.spec.ts
+++ src/app/pages/details/details.component.spec.ts
@@ -1,16 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 import { DetailsComponent } from './details.component';
+import { SeasonService } from 'src/app/services/season.service';
+import { Match } from 'src/app/shared/interfaces/match.interface';
+import { Season } from 'src/app/shared/interfaces/season.interface';
 
 describe('DetailsComponent', () => {
   let component: DetailsComponent;
   let fixture: ComponentFixture<DetailsComponent>;
+  let mockActivatedRoute: any;
+  let mockSeasonService: any;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DetailsComponent ]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    mockActivatedRoute = {
+      paramMap: of({
+        get: (param: string) => {
+          if (param === 'matchId') {
+            return '1'; // mock matchId value
+          }
+          return null;
+        }
+      })
+    };
+
+    mockSeasonService = {
+      getSeason: jasmine.createSpy('getSeason').and.returnValue(of(mockSeasonData()))
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [DetailsComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: SeasonService, useValue: mockSeasonService }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DetailsComponent);
     component = fixture.componentInstance;
@@ -20,4 +44,45 @@ describe('DetailsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  // Helper functions for creating mock data
+  function mockSeasonData(): Season {
+    return {
+      name: 'Premier League 2020/21',
+      matches: mockMatchData()
+    };
+  }
+
+  function mockMatchData(): Match[] {
+    return [
+      {
+        "round": "Matchday 1",
+        "date": "September 11, 2020",
+        "team1": "Burnley FC",
+        "team2": "Manchester United FC",
+        "id": 1
+      },
+      {
+        "round": "Matchday 1",
+        "date": "September 11, 2020",
+        "team1": "Manchester City FC",
+        "team2": "Aston Villa FC",
+        "id": 2
+      },
+      {
+        "round": "Matchday 1",
+        "date": "September 11, 2020",
+        "team1": "Fulham FC",
+        "team2": "Arsenal FC",
+        "score": {
+          "ft": [
+            0,
+            3
+          ]
+        },
+        "id": 3
+      }
+    ];
+  }
 });
--- src/app/services/season.service.spec.ts
+++ src/app/services/season.service.spec.ts
@@ -1,16 +1,27 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { SeasonService } from './season.service';
 
 describe('SeasonService', () => {
-  let service: SeasonService;
+  let seasonService: SeasonService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SeasonService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [SeasonService]
+    });
+
+    seasonService = TestBed.inject(SeasonService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(seasonService).toBeTruthy();
   });
+
 });
